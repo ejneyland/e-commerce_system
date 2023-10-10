ca6070e9a47b5e8c869089774007dbec1558fe0d
Ethan <ethanjneyland@gmail.com>
2023-10-10 14:50:06 +1100

    all object models updated and refined

--- model_order.py
+++ model_order.py
@@ -1,18 +1,22 @@
-from operation_order import OrderOperation
-
 class Order:
 
-    def __init__(self, user_id, prod_id):
-        self.order_id = OrderOperation.generate_order_id()
+    # Order Costructor method
+    def __init__(self, order_id, user_id, prod_id, order_time):
+        self.order_id = order_id
         self.user_id = user_id
         self.prod_id = prod_id
-        self.order_time = OrderOperation.retrieve_time_of_order() or "00-00-0000_00:00:00"
+        self.order_time = order_time or "00-00-0000_00:00:00"
 
+    # Order Str Object method
     def __str__(self):
+        # defines a dictionary of Order attributes, key-value pairs
         order_data = {
             'order_id': {self.order_id},
             'user_id': {self.user_id},
             'prod_id': {self.prod_id},
             'order_time': {self.order_id}
         }
+        # returns a string representation of the Order instance/object
         return str(order_data)
+    
+# ------------------------------------------------------------
--- model_product.py
+++ model_product.py
@@ -1,18 +1,19 @@
-from operation_product import ProductOperation
-
 class Product:
     
-    def __init__(self, prod_model, prod_category, prod_name, prod_current_price, prod_raw_price, prod_discount, prod_likes_count):
-        self.prod_id = ProductOperation.generate_prod_id(), 
-        self.prod_model = prod_model, 
-        self.prod_category = prod_category, 
-        self.prod_name = prod_name, 
-        self.prod_current_price = prod_current_price, 
-        self.prod_raw_price = prod_raw_price, 
-        self.prod_discount = prod_discount, 
+    # Product Constructor method
+    def __init__(self, prod_id, prod_model, prod_category, prod_name, prod_current_price, prod_raw_price, prod_discount, prod_likes_count):
+        self.prod_id = prod_id
+        self.prod_model = prod_model
+        self.prod_category = prod_category
+        self.prod_name = prod_name
+        self.prod_current_price = prod_current_price
+        self.prod_raw_price = prod_raw_price
+        self.prod_discount = prod_discount
         self.prod_likes_count = prod_likes_count
 
+    # Product Str Object method
     def __str__(self):
+        # defines a dictionary of Product attributes, key-value pairs
         prod_data = {
             'prod_id': self.prod_id, 
             'prod_model': self.prod_model, 
@@ -23,4 +24,7 @@ class Product:
             'prod_discount': self.prod_discount, 
             'prod_likes_count': self.prod_likes_count
         }
+        # returns a string representation of the Product instance/object
         return str(prod_data)
+    
+# ------------------------------------------------------------
